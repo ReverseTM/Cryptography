5b3199bb7a2bd7754740a1a2e77ba921a212df8b
Reverse <SteamGamesMy@mail.ru>
2024-03-08 11:38:59 +0300

    Implemented permutation

--- src/main/java/ru/mai/khasanov/cryptography/Main.java
+++ src/main/java/ru/mai/khasanov/cryptography/Main.java
@@ -1,7 +1,11 @@
 package ru.mai.khasanov.cryptography;
 
+import ru.mai.khasanov.cryptography.DES.SecondaryFunctions.SecondaryFunction;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
