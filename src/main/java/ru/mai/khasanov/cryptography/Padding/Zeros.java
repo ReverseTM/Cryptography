267f585e8a1206348448d0b71bb47b6e2e9c81bd
Reverse <SteamGamesMy@mail.ru>
2024-03-20 18:30:20 +0300

    Implement Zeros

--- src/main/java/ru/mai/khasanov/cryptography/Padding/Zeros.java
+++ src/main/java/ru/mai/khasanov/cryptography/Padding/Zeros.java
@@ -2,14 +2,30 @@ package ru.mai.khasanov.cryptography.Padding;
 
 import ru.mai.khasanov.cryptography.interfaces.IPadding;
 
+import java.util.Arrays;
+
 public class Zeros implements IPadding {
     @Override
     public byte[] applyPadding(byte[] data, int blockSize) {
-        return new byte[0];
+        // Длину дополнения
+        int paddingLength = blockSize - (data.length % blockSize);
+        byte[] paddedInput = new byte[data.length + paddingLength];
+        System.arraycopy(data, 0, paddedInput, 0, data.length);
+
+        return paddedInput;
     }
 
     @Override
     public byte[] removePadding(byte[] data) {
-        return new byte[0];
+        int lastNonZeroIndex = data.length - 1;
+        // Находим индекс последнего ненулевого байта
+        for (int i = data.length - 1; i >= 0; i--) {
+            if (data[i] != 0) {
+                lastNonZeroIndex = i;
+                break;
+            }
+        }
+
+        return Arrays.copyOf(data, lastNonZeroIndex + 1);
     }
 }
